8e761d5c60b84256265fe51aef3f0a12763e0318
Leonhard Seidel <leonhardseidel@gmx.de>
2021-11-07 17:25:02 +0100

    added new funcitonality to linedata

--- LineData.cs
+++ LineData.cs
@@ -9,19 +9,50 @@ class LineData
     public string key;
     public bool isTranslated;
     public bool isApproved;
+    public string english;
+    public string translation;
     public string[] comments;
 
-    public LineData(string key)
+    public LineData(string key, string english)
     {
         this.key = key;
+        this.english = english;
     }
 
-    public LineData(string key, bool isTranslated, bool isApproved, string[] comments)
+    public LineData(string key, string english, string translation)
     {
         this.key = key;
+        this.english = english;
+        this.translation = translation;
+    }
+
+    public LineData(string key, string english, string translation, bool isTranslated, bool isApproved, string[] comments)
+    {
+        this.key = key;
+        this.english = english;
+        this.translation = translation;
         this.isTranslated = isTranslated;
         this.isApproved = isApproved;
         this.comments = comments;
+    }
+
+    public void UpdateTranslated(string translation)
+    {
+        this.translation = translation;
+        if(translation != "")
+        {
+            isTranslated = true;
+        }
+    }
+
+    public void UpdateApproval(bool isApproved)
+    {
+        this.isApproved = isApproved;
+    }
+
+    public void AddComment(string Comment)
+    {
+        comments[comments.Length] = Comment;
     }
 
     public override string ToString()
