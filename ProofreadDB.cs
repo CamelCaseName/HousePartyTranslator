e46a881476b0142233326d1c9cccb92a578c0e3c
Leonhard Seidel <leonhardseidel@gmx.de>
2021-11-08 00:22:46 +0100

    Revert "can now write to rinderha.cc instead of local db"
    This reverts commit d64036c70bdf2ab652bb17d0c08b4116833aef66.

--- ProofreadDB.cs
+++ ProofreadDB.cs
@@ -3,51 +3,29 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
-using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace HousePartyTranslator
 {
     static class ProofreadDB
     {
-        private static MySqlConnection sqlConnection;
-        private static MySqlCommand insertApproved;
-        //private static string dbPath;
-        private static readonly string SoftwareVersion = "0.20";
+        private static SqlConnection sqlConnection;
+        private static SqlCommand insertApproved;
+        private static string dbPath;
 
         static ProofreadDB()
         {
-            sqlConnection = new MySqlConnection();
-            //dbPath = Path.GetFullPath(".\\ProofreadDB.mdf");
-            //sqlConnection.ConnectionString = $"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename={dbPath};Integrated Security=True;";
-
-        }
-
-        public static void InitializeDB()
-        {
-            sqlConnection.ConnectionString = "Server=www.rinderha.cc;Uid=user;Pwd=YRKLcAgKIKjFeWHKJFGswaUIHBfrTw;Database=main;";
+            sqlConnection = new SqlConnection();
+            dbPath = Path.GetFullPath(".\\ProofreadDB.mdf");
+            sqlConnection.ConnectionString = $"Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename={dbPath};Integrated Security=True;";
             sqlConnection.Open();
-            Console.WriteLine(sqlConnection.State.ToString());
-            insertApproved = new MySqlCommand("", sqlConnection);
+            insertApproved = new SqlCommand("", sqlConnection);
             Console.WriteLine("DB opened");
-
-            //checking template version
-            MySqlCommand getVersion = new MySqlCommand("SELECT STORY FROM translations WHERE ID = \"version\";", sqlConnection);
-            MySqlDataReader reader = getVersion.ExecuteReader();
-            reader.Read();
-            string DBVersion = reader.GetString(0);
-            reader.Close();
-            if (DBVersion != SoftwareVersion)
-            {
-                MessageBox.Show($"Current software version({SoftwareVersion}) and data version({DBVersion}) differ." +
-                    $" You may acquire the latest version of this program");
-            }
         }
 
         public static bool SetStringAccepted(string id, string fileName = " ", string story = " ", string comments = " ")
         {
-            string insertCommand = @"INSERT INTO translations (id, story, filename, translated, approved, language, comment) 
-                                   VALUES(@id, @story, @fileName, @translated, @approved, @language, @comments)";
+            string insertCommand = @"INSERT INTO dbo.Translations VALUES(@id, @story, @fileName, @translated, @approved, @language, @comments)";
             insertApproved.CommandText = insertCommand;
             insertApproved.Parameters.Clear();
             insertApproved.Parameters.AddWithValue("@id", story + fileName + id);
@@ -57,31 +35,6 @@ namespace HousePartyTranslator
             insertApproved.Parameters.AddWithValue("@approved", 1);
             insertApproved.Parameters.AddWithValue("@language", "de");
             insertApproved.Parameters.AddWithValue("@comments", " ");
-
-
-            if (insertApproved.ExecuteNonQuery() == 1)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        public static bool AddTemplateString(string id, string fileName = " ", string story = " ", string language = "de", string english = " ")
-        {
-            string insertCommand = @"REPLACE INTO translations VALUES(@id, @story, @fileName, @translated, @approved, @language, @comments, @english)";
-            insertApproved.CommandText = insertCommand;
-            insertApproved.Parameters.Clear();
-            insertApproved.Parameters.AddWithValue("@id", story + fileName + id);
-            insertApproved.Parameters.AddWithValue("@story", story);
-            insertApproved.Parameters.AddWithValue("@fileName", fileName);
-            insertApproved.Parameters.AddWithValue("@translated", 1);
-            insertApproved.Parameters.AddWithValue("@approved", 1);
-            insertApproved.Parameters.AddWithValue("@language", "de");
-            insertApproved.Parameters.AddWithValue("@comments", " ");
-            insertApproved.Parameters.AddWithValue("@english", english);
 
 
             if (insertApproved.ExecuteNonQuery() == 1)
