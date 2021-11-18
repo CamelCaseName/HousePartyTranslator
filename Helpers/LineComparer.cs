bdce619970337bc3d1e5aeb1c771040264e1823d
Leonhard Seidel <leonhardseidel@gmx.de>
2021-11-18 19:10:49 +0100

    commented the LineComparer classes

--- Helpers/LineComparer.cs
+++ Helpers/LineComparer.cs
@@ -2,8 +2,21 @@
 
 namespace HousePartyTranslator.Helpers
 {
+    /// <summary>
+    /// This class provides a Compare() method to compare LineData objects with a BGC ID
+    /// </summary>
     class BGCComparer : IComparer<LineData>
     {
+        /// <summary>
+        /// Compares the id of line1 and line2 and returns an indication
+        /// </summary>
+        /// <param name="line1"> The line to compare with line2</param>
+        /// <param name="line2"> line1 is compare to this one</param>
+        /// <returns>
+        /// A signed number indicating the relative values of line1 and line2. Return
+        /// Value Description Less than zero: line1 is before line2. Zero: line1
+        /// is equal to line2. Greater than zero: line1 is after than line2.
+        /// </returns>
         public int Compare(LineData line1, LineData line2)
         {
             //this comparer strips the id of the preceding BGC and then compares the resulting numbers
@@ -11,8 +24,22 @@ namespace HousePartyTranslator.Helpers
         }
     }
 
+    /// <summary>
+    /// This class provides a Compare() methiod for LineData objects with a seminumeric ID, especially hints. 
+    /// Intended for hint sorting, everythin else will be sorted albhabetically.
+    /// </summary>
     class GeneralComparer : IComparer<LineData>
     {
+        /// <summary>
+        /// Compares the id of line1 and line2 and returns an indication
+        /// </summary>
+        /// <param name="line1"> The line to compare with line2</param>
+        /// <param name="line2"> line1 is compare to this one</param>
+        /// <returns>
+        /// A signed number indicating the relative values of line1 and line2. Return
+        /// Value Description Less than zero: line1 is before line2. Zero: line1
+        /// is equal to line2. Greater than zero: line1 is after than line2.
+        /// </returns>
         public int Compare(LineData line1, LineData line2)
         {
             //this comparer checks if the id can be converted to a number, if so remove the u and comapre.
