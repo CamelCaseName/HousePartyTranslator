20745f305c9a9880b8e3cbfe7a40c8b05ed4d203
Leonhard Seidel <leonhardseidel@gmx.de>
2022-07-17 18:53:33 +0200

    Updated version

--- Managers/SoftwareVersionManager.cs
+++ Managers/SoftwareVersionManager.cs
@@ -9,7 +9,7 @@ namespace HousePartyTranslator.Managers
 {
     static class SoftwareVersionManager
     {
-        public static readonly string LocalVersion = "0.5.1.0";
+        public static readonly string LocalVersion = "0.5.2.0";
         public static string LatestGithubVersion;
         public static bool UpdatePending = false;
         private static readonly HttpClient client = new HttpClient();
