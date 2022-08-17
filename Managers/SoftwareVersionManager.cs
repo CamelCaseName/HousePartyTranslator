c2db8a5a916ad44ef88217cdd76fc3d9bf714690
Leonhard Seidel <leonhardseidel@gmx.de>
2022-08-17 17:50:57 +0200

    Increase version for new release

--- Managers/SoftwareVersionManager.cs
+++ Managers/SoftwareVersionManager.cs
@@ -9,7 +9,7 @@ namespace HousePartyTranslator.Managers
 {
     static class SoftwareVersionManager
     {
-        public static readonly string LocalVersion = "0.5.4.4";
+        public static readonly string LocalVersion = "0.5.4.5";
         public static string LatestGithubVersion;
         public static bool UpdatePending = false;
         private static readonly HttpClient client = new HttpClient();
