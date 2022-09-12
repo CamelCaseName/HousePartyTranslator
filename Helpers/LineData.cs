7a711a8dc10ba3fa46bb959f7edb0091d8ad1825
Leonhard Seidel <leonhardseidel@gmx.de>
2022-09-12 17:03:53 +0200

    Removed some unused constructors

--- Helpers/LineData.cs
+++ Helpers/LineData.cs
@@ -15,25 +15,6 @@ internal sealed class LineData
 
     public LineData() { }
 
-    public LineData(string id, string story, string filename, StringCategory category, bool isApproved)
-    {
-        ID = id;
-        Story = story;
-        FileName = filename;
-        Category = category;
-        IsApproved = isApproved;
-    }
-
-    public LineData(string id, string story, string filename, StringCategory category, bool isApproved, bool isTranslated)
-    {
-        ID = id;
-        Story = story;
-        FileName = filename;
-        Category = category;
-        IsApproved = isApproved;
-        IsTranslated = isTranslated;
-    }
-
     public LineData(string id, string story, string filename, StringCategory category)
     {
         ID = id;
@@ -63,18 +44,6 @@ internal sealed class LineData
         IsTranslated = translation.Length > 1;
     }
 
-    public LineData(string id, string story, string filename, StringCategory category, bool isApproved, string english, string translation)
-    {
-        ID = id;
-        Story = story;
-        FileName = filename;
-        Category = category;
-        IsApproved = isApproved;
-        TemplateString = english;
-        TranslationString = translation;
-        IsTranslated = translation.Length > 1;
-    }
-
     public LineData(string id, string story, string filename, StringCategory category, string translation)
     {
         ID = id;
@@ -82,51 +51,6 @@ internal sealed class LineData
         FileName = filename;
         Category = category;
         TranslationString = translation;
-    }
-
-    public LineData(string id, string story, string filename, StringCategory category, string english, string translation, bool isApproved, bool isTranslated, string[] comments)
-    {
-        ID = id;
-        Story = story;
-        FileName = filename;
-        Category = category;
-        TemplateString = english;
-        TranslationString = translation;
-        IsTranslated = isTranslated;
-        IsApproved = isApproved;
-        Comments = comments;
-    }
-
-    public void UpdateTranslated(string translation)
-    {
-        TranslationString = translation;
-        if (translation != "")
-        {
-            IsTranslated = true;
-        }
-    }
-
-    public void UpdateApproval(bool isApproved)
-    {
-        IsApproved = isApproved;
-    }
-
-    public void AddComment(string Comment)
-    {
-        if (Comments != null)
-        {
-            string[] tempArray = Comments;
-            Comments = new string[tempArray.Length + 1];
-            for (int i = 0; i < tempArray.Length; i++)
-            {
-                Comments[i] = tempArray[i];
-            }
-            Comments[tempArray.Length] = Comment;
-        }
-        else
-        {
-            Comments = new string[1] { Comment };
-        }
     }
 
     public override string ToString()
