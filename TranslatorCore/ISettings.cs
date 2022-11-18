553941e738074b54fd92e664e8add0ed04a9a851
Leonhard Seidel <leonhardseidel@gmx.de>
2022-11-18 21:44:22 +0100

    added a settigns interface

---
+++ TranslatorCore/ISettings.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TranslatorCore
+{
+    internal interface ISettings
+    {
+        bool AdvancedModeEnabled { get; set; }
+        bool AllowCustomStories { get; set; }
+        bool AlsoSaveToGame { get; set; }
+        bool AskForSaveDialog { get; set; }
+        bool AutoLoadRecent { get; set; }
+        bool AutoLoadRecentIndex { get; set; }
+        bool AutoSave { get; set; }
+        bool AutoTranslate { get; set; }
+        bool DoDiscordRichPresence { get; set; }
+        bool UseFalseFolder { get; set; }
+        int RecentIndex { get; set; }
+        string DbPassword { get; set; }
+        string DbUserName { get; set; }
+        string FileVersion { get; set; }
+        string Language { get; set; }
+        string Recents0 { get; set; }
+        string Recents1 { get; set; }
+        string Recents2 { get; set; }
+        string Recents3 { get; set; }
+        string Recents4 { get; set; }
+        string TemplatePath { get; set; }
+        string TranslationPath { get; set; }
+        TimeSpan AutoSaveInterval { get; set; }
+    }
+}
