c033de5fbf1671d2a0ec9486c7949a985fa71cd4
Leonhard Seidel <leonhardseidel@gmx.de>
2022-11-29 19:18:50 +0100

    Folder dialog implementation for windows forms done

--- TranslatorAdmin/InterfaceImpls/WinFolderDialog.cs
+++ TranslatorAdmin/InterfaceImpls/WinFolderDialog.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Translator.Helpers;
+using Translator.UICompatibilityLayer;
 
 namespace TranslatorAdmin.InterfaceImpls
 {
-    internal class WinFolderDialog
+    internal class WinFolderDialog : IFolderDialog
     {
+        private readonly FolderBrowserDialog dialog = new() { UseDescriptionForTitle = true , RootFolder = Environment.SpecialFolder.Recent};
+        public string SelectedFolderPath { get => dialog.SelectedPath; set => dialog.SelectedPath = value; }
+        public string Text { get => dialog.Description; set => dialog.Description = value; }
+
+        public PopupResult ShowDialog() => dialog.ShowDialog().ToPopupResult();
     }
 }
