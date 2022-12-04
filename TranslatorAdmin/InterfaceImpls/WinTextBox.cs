504b8677d8575a31f79e2fdeb64f62042deb514e
Leonhard Seidel <leonhardseidel@gmx.de>
2022-12-04 18:10:43 +0100

    wintextbox implementation done

--- TranslatorAdmin/InterfaceImpls/WinTextBox.cs
+++ TranslatorAdmin/InterfaceImpls/WinTextBox.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace TranslatorAdmin.InterfaceImpls
 {
-    internal class WinTextBox
+    internal class WinTextBox : TextBox, Translator.UICompatibilityLayer.ITextBox
     {
+        public int SelectionEnd
+        {
+            get => base.SelectionStart + SelectionLength;
+            set { if (SelectionStart < SelectionEnd) SelectionLength = value - SelectionStart; else throw new ArgumentOutOfRangeException(nameof(SelectionEnd), "End has to be after SelectionStart"); }
+        }
+        public new int SelectionStart { get => base.SelectionStart; set => base.SelectionStart = value; }
+        public new string Text { get => base.Text; set => base.Text = value; }
+
+        public new void Focus() => base.Focus();
     }
 }
