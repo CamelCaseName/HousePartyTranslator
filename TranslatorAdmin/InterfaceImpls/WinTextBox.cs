2b58f7ba41b89c8301ed97865e89b14aa763cb69
Leonhard Seidel <leonhardseidel@gmx.de>
2022-12-04 18:11:44 +0100

    cleaned up namespaces and using

--- TranslatorAdmin/InterfaceImpls/WinTextBox.cs
+++ TranslatorAdmin/InterfaceImpls/WinTextBox.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Translator.UICompatibilityLayer;
 
 namespace TranslatorAdmin.InterfaceImpls
 {
-    internal class WinTextBox : TextBox, Translator.UICompatibilityLayer.ITextBox
+    internal class WinTextBox : TextBox, ITextBox
     {
         public int SelectionEnd
         {
