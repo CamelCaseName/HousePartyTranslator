d576653b7acbbbfff75024734e3444dddc8dcf2a
Leonhard Seidel <leonhardseidel@gmx.de>
2023-01-08 18:23:36 +0100

    learning blazor

--- TranslatorWebUser/Program.cs
+++ TranslatorWebUser/Program.cs
@@ -1,8 +1,5 @@
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using TranslatorWebUser.Areas.Identity;
 using TranslatorWebUser.Data;
---
+++ TranslatorWebUser/TodoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace TranslatorWebUser
+{
+    public class TodoItem
+    {
+        public string? Title { get; set; }
+        public bool IsDone { get; set; } = false;
+    }
+}
