426373c898929701ee9adff56a0554d3d1ddeacf
Leonhard Seidel <leonhardseidel@gmx.de>
2023-05-15 10:03:52 +0200

    moved all filter ui building to respetive class

--- TranslatorAdmin/Explorer/GraphingEngine.cs
+++ TranslatorAdmin/Explorer/GraphingEngine.cs
@@ -355,7 +355,10 @@ namespace Translator.Desktop.Explorer
             if (!ExtendedInfoComponents.TryGetValue(node.DataType, out var box))
             {
                 //create new and cache, then display
-                box = GetDisplayComponentsForType(node.Data, node.DataType);
+                box = FilterUIBuilder.GetDisplayComponentsForType(
+                    node.Data, 
+                    node.DataType, 
+                    new Size(Math.Clamp(NodeInfoLabel.ClientRectangle.Width, 340, 900), Explorer.ClientRectangle.Height - Explorer.Grapher.NodeInfoLabel.Height - 10));
                 Explorer.Controls.Add(box);
                 ExtendedInfoComponents.Add(node.DataType, box);
             }
@@ -369,113 +372,6 @@ namespace Translator.Desktop.Explorer
             FilterUIBuilder.SetEditableStates(box);
             box.BringToFront();
             box.Visible = true;
-        }
-
-        private GroupBox GetDisplayComponentsForType(object data, Type dataType)
-        {
-            var box = new GroupBox()
-            {
-                Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Bottom,
-                AutoSize = true,
-                ForeColor = Utils.brightText,
-                Name = dataType.Name + "DisplayBox",
-                TabIndex = 10,
-                TabStop = false,
-                MaximumSize = new Size(Math.Clamp(NodeInfoLabel.ClientRectangle.Width, 340, 900), Explorer.ClientRectangle.Height - Explorer.Grapher.NodeInfoLabel.Height - 10),
-                Text = dataType.Name,
-                Visible = false
-            };
-            box.SuspendLayout();
-            var grid = new TableLayoutPanel()
-            {
-                Name = dataType.Name + "ValueTable",
-                GrowStyle = TableLayoutPanelGrowStyle.AddRows,
-                AutoSize = true,
-                TabStop = false,
-                CellBorderStyle = TableLayoutPanelCellBorderStyle.Single,
-                ColumnCount = 2,
-                Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right,
-                Dock = DockStyle.Fill,
-                AutoScroll = true,
-            };
-            box.Controls.Add(grid);
-
-            foreach (var property in dataType.GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
-            {
-                try
-                {
-                    Type valueType = property.PropertyType;
-                    object? value = property.GetValue(data);
-                    //string
-                    if (valueType == typeof(string))
-                    {
-                        var label = new Label() { AutoSize = true, Text = property.Name, Name = property.Name + "Label", Dock = DockStyle.Fill, ForeColor = Utils.brightText };
-                        grid.Controls.Add(label);
-                        var text = new TextBox() { Text = (string?)value ?? string.Empty, Name = property.Name + "TextBox", Multiline = true, WordWrap = true, Dock = DockStyle.Fill, ReadOnly = FilterUIBuilder.ReadOnly, };
-                        text.TextChanged += (object? sender, EventArgs e) => FilterUIBuilder.TextBoxSetValue(sender, property, data);
-                        grid.Controls.Add(text);
-                    }
-                    //numbers
-                    else if (valueType == typeof(int) || valueType == typeof(float))
-                    {
-                        var label = new Label() { AutoSize = true, Text = property.Name, Name = property.Name + "Label", Dock = DockStyle.Fill, ForeColor = Utils.brightText };
-                        grid.Controls.Add(label);
-                        var numeric = new NumericUpDown() { Minimum = int.MinValue, Maximum = int.MaxValue, Value = Convert.ToDecimal(value), Name = property.Name + "Numeric", ReadOnly = FilterUIBuilder.ReadOnly, InterceptArrowKeys = true };
-                        if (valueType == typeof(int)) numeric.ValueChanged += (object? sender, EventArgs e) => FilterUIBuilder.NumericIntSetValue(sender, property, data);
-                        else numeric.ValueChanged += (object? sender, EventArgs e) => FilterUIBuilder.NumericFloatSetValue(sender, property, data);
-                        grid.Controls.Add(numeric);
-                    }
-                    //bool
-                    else if (valueType == typeof(bool))
-                    {
-                        var label = new Label() { AutoSize = true, Text = property.Name, Name = property.Name + "Label", Dock = DockStyle.Fill, ForeColor = Utils.brightText };
-                        grid.Controls.Add(label);
-                        var checkBox = new CheckBox { Checked = Convert.ToBoolean(value), Name = property.Name + "Checkbox", Enabled = !FilterUIBuilder.ReadOnly };
-                        checkBox.CheckedChanged += (object? sender, EventArgs e) => FilterUIBuilder.CheckBoxSetValue(sender, property, data);
-                        grid.Controls.Add(checkBox);
-                    }
-                    //nullable enum
-                    else if (valueType.GenericTypeArguments.Length > 0)
-                    {
-                        if (valueType.GenericTypeArguments[0].IsEnum)
-                        {
-                            var label = new Label() { AutoSize = true, Text = property.Name, Name = property.Name + "Label", Dock = DockStyle.Fill, ForeColor = Utils.brightText };
-                            grid.Controls.Add(label);
-                            var dropDown = new ComboBox() { Name = property.Name + "ComboBox", DropDownStyle = ComboBoxStyle.DropDownList, Dock = DockStyle.Fill, Anchor = AnchorStyles.Left };
-                            foreach (var enumItem in Enum.GetNames(valueType.GenericTypeArguments[0]))
-                            {
-                                dropDown.Items.Add(enumItem);
-                            }
-                            dropDown.SelectedItem = value?.ToString();
-                            dropDown.SelectedValueChanged += (object? sender, EventArgs e) => FilterUIBuilder.DropDownNullableSetValue(sender, property, data);
-                            dropDown.Enabled = !FilterUIBuilder.ReadOnly;
-                            grid.Controls.Add(dropDown);
-                        }
-                    }
-                    //enum
-                    else if (valueType.IsEnum)
-                    {
-                        var label = new Label() { AutoSize = true, Text = property.Name, Name = property.Name + "Label", Dock = DockStyle.Fill, ForeColor = Utils.brightText };
-                        grid.Controls.Add(label);
-                        var dropDown = new ComboBox() { Name = property.Name + "ComboBox", DropDownStyle = ComboBoxStyle.DropDownList, Dock = DockStyle.Fill, Anchor = AnchorStyles.Left };
-                        foreach (var enumItem in Enum.GetNames(valueType))
-                        {
-                            dropDown.Items.Add(enumItem);
-                        }
-                        dropDown.SelectedItem = value?.ToString();
-                        dropDown.SelectedValueChanged += (object? sender, EventArgs e) => FilterUIBuilder.DropDownSetValue(sender, property, data);
-                        dropDown.Enabled = !FilterUIBuilder.ReadOnly;
-                        grid.Controls.Add(dropDown);
-                    }
-                }
-                catch (Exception e)
-                {
-                    LogManager.Log(e.Message);
-                }
-            }
-
-            box.ResumeLayout();
-            return box;
         }
 
         private void DisplayNodeInfoHandler(object sender, ClickedNodeChangeArgs e)
