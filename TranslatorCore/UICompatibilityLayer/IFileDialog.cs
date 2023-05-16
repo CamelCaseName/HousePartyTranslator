6c9fcac5cd35781c2e8e7d99df259187c968b8c0
Leonhard Seidel <leonhardseidel@gmx.de>
2023-05-16 15:51:39 +0200

    added new field to filedialog

--- TranslatorCore/UICompatibilityLayer/IFileDialog.cs
+++ TranslatorCore/UICompatibilityLayer/IFileDialog.cs
@@ -4,13 +4,19 @@ namespace Translator.Core.UICompatibilityLayer
 {
     public interface IFileDialog
     {
-        bool MultiSelect { get; set; }
-        string FileName { get; set; }
+        //constructer in this order
+        string Title { get; set; }
         string Filter { get; set; }
         string InitialDirectory { get; set; }
+        string FileName { get; set; }
+
+        //other settings
+        bool MultiSelect { get; set; }
+        bool CheckFileExists { get; set; }
+
+        //returned values
         string SelectedPath { get; }
         string[] SelectedPaths { get; }
-        string Title { get; set; }
         PopupResult ShowDialog();
     }
 }
