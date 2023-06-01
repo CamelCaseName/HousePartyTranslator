03b0b80abe4fdf0030acc3acbbabbba0ab1b5737
Leonhard Seidel <leonhardseidel@gmx.de>
2023-06-01 10:28:19 +0200

    added argument checking to the searchtoolstriptextbox

--- TranslatorAdmin/UI/Components/SearchToolStripTextBox.cs
+++ TranslatorAdmin/UI/Components/SearchToolStripTextBox.cs
@@ -9,7 +9,50 @@ namespace Translator.Desktop.UI.Components
 {
     internal sealed class SearchToolStripTextBox : WinToolStripTextBox
     {
-        public int TotalSearchResults { get; set; }
-        public int CurrentSearchResult { get; set; }
+        public int TotalSearchResults
+        {
+            get
+            {
+                return _totalSearchResults;
+            }
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        nameof(TotalSearchResults),
+                        $"{nameof(TotalSearchResults)} cannot be negative");
+                }
+                else
+                {
+                    _totalSearchResults = value;
+                }
+            }
+        }
+        private int _totalSearchResults = 0;
+
+        public int CurrentSearchResults
+        {
+            get
+            {
+                return _currentSearchResults;
+            }
+
+            set
+            {
+                if (value < 0 && value >= TotalSearchResults)
+                {
+                    throw new ArgumentOutOfRangeException(
+                        nameof(CurrentSearchResults),
+                        $"{nameof(CurrentSearchResults)} cannot be negative or greater than the total number of results");
+                }
+                else
+                {
+                    _currentSearchResults = value;
+                }
+            }
+        }
+        private int _currentSearchResults = 0;
     }
 }
